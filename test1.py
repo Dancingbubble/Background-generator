7d8cd8c75b68e9e039fdde0b269b91f3f3dfb53c
dancingbubble <fulanzhangcarrie@gmai.com>
2023-01-09 22:02:15 -0500

    add

---
+++ test1.py
@@ -0,0 +1,13 @@
+from translate import Translator
+translator=Translator(to_lan='zh')
+try:
+    with open('./test.txt', mode='r') as my_file:
+        text=my_file.read()
+        translation=translator.translate(text)
+        with open('./test_zh.txt', mode='w') as my_file1:
+            my_file1.write(translation)
+        print(translation)
+except FileNotFoundError as err:
+    print('check your file path')
+
+
